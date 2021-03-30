ba73775cd35335ec205d2ec3ddf86626250e2479
takum.iohtsubo <takumi061174@gmail.com>
2021-03-30 11:59:01 +0000

    add column

--- app/Http/Controllers/TasksController.php
+++ app/Http/Controllers/TasksController.php
@@ -39,7 +39,9 @@ class TasksController extends Controller
      */
     public function store(Request $request)
     {
-         $task = new Task;
+        $request->validate(['status' => 'required|10', 'content' => 'required',]);
+        $task = new Task;
+        $task->status = $request->status;
         $task->content = $request->content;
         $task->save();
 
@@ -81,7 +83,10 @@ class TasksController extends Controller
      */
     public function update(Request $request, $id)
     {
+        $request->validate(['status' => 'required|max:10', 'content' => 'required',]);
+       
         $task = Task::findOrFail($id);
+        $task->status = $request->status;
         $task->content = $request->content;
         $task->save();
 
